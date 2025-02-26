e72faa247716e79ef2e970edf0d0980c319225d7
Brian Wing <bwing@anaconda.com>
2025-02-26 09:14:14 -0500

    Update to v13.6.0

--- recipe/scripts/deactivate.bat
+++
@@ -1,7 +0,0 @@
-@REM Restore previous CUDA_PATH env var if it was set
-
-@set "CUDA_PATH="
-@if defined CONDA_CUPY_CUDA_PATH (
-  set "CUDA_PATH=%CONDA_CUPY_CUDA_PATH%"
-  set "CONDA_CUPY_CUDA_PATH="
-)
